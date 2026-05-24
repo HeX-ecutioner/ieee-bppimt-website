775a74469077536fe926fbe4a5ba06bd95d43664
Sagnik Maitra <sagnikmaitra01@gmail.com>
2026-05-24 22:36:57 +0530

    Add Team component with data and integrate routing for team page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Stats from './components/Stats'
@@ -5,20 +6,34 @@ import About from './components/About'
 import Events from './components/Events'
 import CtaBanner from './components/CtaBanner'
 import Footer from './components/Footer'
+import Team from './components/Team'
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Stats />
+      <About />
+      <Events />
+      <CtaBanner />
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-ieee-light">
-      <Navbar />
-      <main className="flex-grow">
-        <Hero />
-        <Stats />
-        <About />
-        <Events />
-        <CtaBanner />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="min-h-screen flex flex-col bg-ieee-light">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/team" element={<Team />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
